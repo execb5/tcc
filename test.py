7750331289db67e8f16894aa5bde8d2d8c1eb187
Daniel Amarante <daniel.amarante2@gmail.com>
2017-04-02 20:56:58 -0300

    Add dilation

--- test.py
+++ test.py
@@ -5,14 +5,21 @@ def main():
     image = cv2.imread('test_image_2.png')
     gray_image = convert_grayscale(image)
     cv2.imwrite('grayscale.jpg', gray_image)
+
     equalized_image = apply_histogram_equalization(gray_image)
     cv2.imwrite('histogram_eq.jpg', equalized_image)
+
     # opened_image = apply_morphological_openning(equalized_image)
     # cv2.imwrite('opened_image.jpg', opened_image)
+
     binarized_image = binarize_image(equalized_image)
     cv2.imwrite('binarized_image.jpg', binarized_image)
+
     sobel_image = apply_sobel_edge_detection(binarized_image)
     cv2.imwrite('sobel_image.jpg', sobel_image)
+
+    dilated_image = apply_dilation(sobel_image)
+    cv2.imwrite('dilated_image.jpg', dilated_image)
 
 def convert_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -39,6 +46,10 @@ def apply_sobel_edge_detection(image):
     abs_grad_x = cv2.convertScaleAbs(grad_x)
     abs_grad_y = cv2.convertScaleAbs(grad_y)
     return cv2.addWeighted(abs_grad_x,0.5,abs_grad_y,0.5,0)
+
+def apply_dilation(image):
+    kernel = numpy.ones((5,5),numpy.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
 
 if __name__ == "__main__":
     main()
