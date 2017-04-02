8c94cde65c8d2f15e40f5b896bf19d3797bda4e5
Daniel Amarante <daniel.amarante2@gmail.com>
2017-04-02 20:01:23 -0300

    Binarize image

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ def main():
     cv2.imwrite('histogram_eq.jpg', equalized_image)
     # opened_image = apply_morphological_openning(equalized_image)
     # cv2.imwrite('opened_image.jpg', opened_image)
+    binarized_image = binarize_image(equalized_image)
+    cv2.imwrite('binarized_image.jpg', binarized_image)
 
 def convert_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -20,6 +22,10 @@ def apply_histogram_equalization(image):
 def apply_morphological_openning(image):
     kernel = numpy.ones((5,5),numpy.uint8)
     return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+def binarize_image(image):
+    (thresh, binarized_image) = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    return binarized_image
 
 if __name__ == "__main__":
     main()
