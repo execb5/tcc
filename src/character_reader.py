cbc7f4e2504d23e088143de08319c2e280f86d93
Daniel Amarante <daniel.amarante2@gmail.com>
2017-05-14 21:43:20 -0300

    Put tesseract options in instance variable

--- src/character_reader.py
+++ src/character_reader.py
@@ -3,16 +3,19 @@ import pytesseract
 
 
 class CharacterReader:
+    letter_options = "-psm 10 -c tessedit_char_whitelist=QWERTYUIOPASDFGHJKLZXCVBNM"
+    number_options = "-psm 10 -c tessedit_char_whitelist=1234567890"
+
     def __init__(self):
         pass
 
     def read_characters(self, characters):
         i = 0
+
         for _, character in characters:
             image = Image.fromarray(character)
             if i < 3:
-                print pytesseract.image_to_string(image,
-                                                  config="-psm 10 -c tessedit_char_whitelist=QWERTYUIOPASDFGHJKLZXCVBNM")
+                print pytesseract.image_to_string(image, config=self.letter_options)
             else:
-                print pytesseract.image_to_string(image, config="-psm 10 -c tessedit_char_whitelist=1234567890")
+                print pytesseract.image_to_string(image, config=self.number_options)
             i = i + 1
