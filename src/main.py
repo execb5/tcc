f2d3b13dd87e9f7ebbf49d9c03c41af63aca2f2b
Matthias Nunes <matthiasnunes@gmail.com>
2017-05-20 19:57:08 -0300

    pass arguments through cli

--- src/main.py
+++ src/main.py
@@ -1,44 +1,47 @@
 import cv2
 import sys
+import imghdr
 
 from plate_extractor import *
 from plate_cleaner import *
 from character_extractor import *
 from character_reader import *
 
-VIDEO = True
-
-plate_extractor = PlateExtractor()
-plate_cleaner = PlateCleaner()
-character_extractor = CharacterExtractor()
-character_reader = CharacterReader()
-
 
 def main():
-    if VIDEO:
-        cap = cv2.VideoCapture("./video.mp4")
-        pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        while True:
-            _, frame = cap.read()
-            try:
-                aux(frame)
-            except IOError as (errno, strerror):
-                print "I/O error({0}): {1}".format(errno, strerror)
-            except ValueError:
-                print "Could not convert data to an integer."
-            except:
-                print "Unexpected error:", sys.exc_info()[0]
-                raise
+    for index, item in enumerate(sys.argv):
+        if index == 0:
+            continue
+        print imghdr.what(item)
+        if imghdr.what(item) in ['jpg','png', 'JPEG', 'jpeg', 'JPG', 'gif']:
+            image = cv2.imread(item)
+            process_frame_or_image(image)
+        else:
+            cap = cv2.VideoCapture(item)
             pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-            if __debug__:
-                print str(pos_frame) + " frames"
-            if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-                break
-    else:
-        image = cv2.imread("full_car.jpg")
-        aux(image)
+            while True:
+                _, frame = cap.read()
+                try:
+                    process_frame_or_image(frame)
+                except IOError as (errno, strerror):
+                    print "I/O error({0}): {1}".format(errno, strerror)
+                except ValueError:
+                    print "Could not convert data to an integer."
+                except:
+                    print "Unexpected error:", sys.exc_info()[0]
+                    raise
 
-def aux(image):
+                pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+                if __debug__:
+                    print str(pos_frame) + " frames"
+                if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                    break
+
+def process_frame_or_image(image):
+    plate_extractor = PlateExtractor()
+    plate_cleaner = PlateCleaner()
+    character_extractor = CharacterExtractor()
+    character_reader = CharacterReader()
     candidates = plate_extractor.extract_plate_candidates(image)
     for candidate in candidates:
         prepared_plate = plate_cleaner.clean_plate(candidate)
