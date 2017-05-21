730c93f7f05fe4325c5c3440cd236fc37e58453b
Daniel Amarante <daniel.amarante2@gmail.com>
2017-05-20 22:36:13 -0300

    Makes ocr works with very very messy code #lacratop

--- src/main.py
+++ src/main.py
@@ -11,16 +11,23 @@ def main():
     plate_cleaner = PlateCleaner()
     character_extractor = CharacterExtractor()
     candidates = plate_extractor.extract_plate_candidates(image)
-    for candidate in candidates:
+    for candidate in reversed(candidates):
         prepared_plate = plate_cleaner.clean_plate(candidate)
         characters = character_extractor.extract_characters(prepared_plate)
 
-        samples = np.loadtxt('char_samples.data', np.float32)
-        responses = np.loadtxt('char_responses.data', np.float32)
-        responses = responses.reshape((responses.size, 1))
+        letter_samples = np.loadtxt('letters_samples.data', np.float32)
+        letter_responses = np.loadtxt('letters_responses.data', np.float32)
+        letter_responses = letter_responses.reshape((letter_responses.size, 1))
 
-        model = cv2.ml.KNearest_create()
-        model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+        letter_model = cv2.ml.KNearest_create()
+        letter_model.train(letter_samples, cv2.ml.ROW_SAMPLE, letter_responses)
+
+        number_samples = np.loadtxt('numbers_samples.data', np.float32)
+        number_responses = np.loadtxt('numbers_responses.data', np.float32)
+        number_responses = number_responses.reshape((number_responses.size, 1))
+
+        number_model = cv2.ml.KNearest_create()
+        number_model.train(number_samples, cv2.ml.ROW_SAMPLE, number_responses)
 
         i = 0
         plate_chars = ""
@@ -28,11 +35,14 @@ def main():
             resized = resize_image(char_img)
             small_img = cv2.resize(resized, (100, 100))
             cv2.imwrite('penis%s.png' % i, small_img)
-            i = i + 1
             small_img = small_img.reshape((1, 10000))
             small_img = np.float32(small_img)
-            retval, results, neigh_resp, dists = model.findNearest(small_img, k=3)
+            if i < 3:
+                retval, results, neigh_resp, dists = letter_model.findNearest(small_img, k=1)
+            else:
+                retval, results, neigh_resp, dists = number_model.findNearest(small_img, k=1)
             plate_chars += str(chr((results[0][0])))
+            i = i + 1
 
         print("Licence plate: %s" % plate_chars)
 
