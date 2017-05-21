9973fc879ba390fd482ec6d5fc5d351b293ba374
Daniel Amarante <daniel.amarante2@gmail.com>
2017-05-20 23:49:06 -0300

    Remove code from plate cleaner that is not nessessaru

--- src/plate_cleaner.py
+++ src/plate_cleaner.py
@@ -25,36 +25,3 @@ class PlateCleaner:
             print "binarization " + str(end_time) + " seconds"
 
         return fill_binary
-
-        # if __debug__:
-        #     start_time = time.time()
-        # dilated_plate = apply_dilation(fill_binary, 11)
-        # if __debug__:
-        #     cv2.imwrite('../output/a3dilated_image.jpg', dilated_plate)
-        #     end_time = time.time() - start_time
-        #     print "dilation " + str(end_time) + " seconds"
-        #
-        # if __debug__:
-        #     start_time = time.time()
-        # fill_eroded = apply_erosion(dilated_plate, 20)
-        # if __debug__:
-        #     cv2.imwrite('../output/a4fill_eroded.jpg', fill_eroded)
-        #     end_time = time.time() - start_time
-        #     print "erosion " + str(end_time) + " seconds"
-        #
-        # if __debug__:
-        #     start_time = time.time()
-        # dilated_plate = apply_dilation(fill_eroded, 22)
-        # if __debug__:
-        #     cv2.imwrite('../output/a5dilated_again_image.jpg', dilated_plate)
-        #     end_time = time.time() - start_time
-        #     print "dilation " + str(end_time) + " seconds"
-        #
-        # if __debug__:
-        #     start_time = time.time()
-        # fill_eroded = apply_erosion(dilated_plate, 22)
-        # if __debug__:
-        #     cv2.imwrite('../output/a6fill_eroded_again.jpg', fill_eroded)
-        #     end_time = time.time() - start_time
-        #     print "erosion " + str(end_time) + " seconds"
-        # return fill_eroded
