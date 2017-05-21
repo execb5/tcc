c393cd03b8eb0f697333024620a10300a3976394
Daniel Amarante <daniel.amarante2@gmail.com>
2017-05-20 23:24:32 -0300

    Fix path to learning data

--- src/main.py
+++ src/main.py
@@ -15,15 +15,15 @@ def main():
         prepared_plate = plate_cleaner.clean_plate(candidate)
         characters = character_extractor.extract_characters(prepared_plate)
 
-        letter_samples = np.loadtxt('letters_samples.data', np.float32)
-        letter_responses = np.loadtxt('letters_responses.data', np.float32)
+        letter_samples = np.loadtxt('../learning_data/letters_samples.data', np.float32)
+        letter_responses = np.loadtxt('../learning_data/letters_responses.data', np.float32)
         letter_responses = letter_responses.reshape((letter_responses.size, 1))
 
         letter_model = cv2.ml.KNearest_create()
         letter_model.train(letter_samples, cv2.ml.ROW_SAMPLE, letter_responses)
 
-        number_samples = np.loadtxt('numbers_samples.data', np.float32)
-        number_responses = np.loadtxt('numbers_responses.data', np.float32)
+        number_samples = np.loadtxt('../learning_data/numbers_samples.data', np.float32)
+        number_responses = np.loadtxt('../learning_data/numbers_responses.data', np.float32)
         number_responses = number_responses.reshape((number_responses.size, 1))
 
         number_model = cv2.ml.KNearest_create()
