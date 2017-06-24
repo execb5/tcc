3dec7a80de25e7c00ae160c162690b3574934e94
Matthias Nunes <matthiasnunes@gmail.com>
2017-06-24 22:21:34 +0000

    adds script for collecting results

---
+++ script.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/zsh
+
+#files=(input/photos/*)
+#for i in `seq 10`
+#do
+	#time python -O src/main.py ${files[RANDOM % ${#files[@]}]}
+#done
+
+for i in `seq 10`
+do
+	time python -O src/main.py input/photos/IXJ-6605.JPG
+done
