8bdc7d4a05e37237035cdd959b9c751d71187291
Matthias Nunes <matthiasnunes@gmail.com>
2017-06-24 19:32:54 -0300

    adds results for 1 image runs

---
+++ mac_results.exs
@@ -0,0 +1,12 @@
+3.164
+|> Kernel.+(3.233)
+|> Kernel.+(3.318)
+|> Kernel.+(3.220)
+|> Kernel.+(3.150)
+|> Kernel.+(3.224)
+|> Kernel.+(3.253)
+|> Kernel.+(3.225)
+|> Kernel.+(3.090)
+|> Kernel.+(3.043)
+|> Kernel./(10)
+|> IO.puts
--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
-#! /usr/bin/zsh
+#! /usr/local/bin/zsh
 
-#files=(input/photos/*)
-#for i in `seq 10`
-#do
-	#time python -O src/main.py ${files[RANDOM % ${#files[@]}]}
-#done
-
+files=(input/photos/*)
 for i in `seq 10`
 do
-	time python -O src/main.py input/photos/IXJ-6605.JPG
+	time python -O src/main.py ${files[RANDOM % ${#files[@]}]}
 done
+
+#for i in `seq 10`
+#do
+	#time python -O src/main.py input/photos/IXJ-6605.JPG
+#done
