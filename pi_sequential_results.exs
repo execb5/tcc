5958710971a6695c8309fc688782602d3dfecd10
Matthias Nunes <matthiasnunes@gmail.com>
2017-06-25 00:23:50 +0000

    adds sequential results for 10 executions on pi

---
+++ pi_sequential_results.exs
@@ -0,0 +1,160 @@
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      462.51s user 4.87s system 135% cpu 5:46.17 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      456.05s user 4.83s system 134% cpu 5:41.94 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      455.09s user 4.88s system 134% cpu 5:40.93 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      455.85s user 4.93s system 135% cpu 5:40.64 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      458.06s user 4.80s system 134% cpu 5:44.14 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      461.73s user 4.87s system 134% cpu 5:46.69 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      462.83s user 5.17s system 135% cpu 5:46.36 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      459.00s user 4.73s system 134% cpu 5:43.85 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      453.41s user 4.61s system 135% cpu 5:38.06 total
+Didn't find anything in photo of plate IPN-5186 :(
+ITA-0204 == ITA-0204 ? True
+Didn't find anything in photo of plate IUZ-5286 :(
+IVA-5837 == IVA-5837 ? True
+IVG-3501 == IVG-3501 ? True
+Didn't find anything in photo of plate IWS-5800 :(
+IWU-3070 == IWU-3070 ? True
+IXJ-6605 == IXJ-6605 ? True
+Didn't find anything in photo of plate IYA-3344 :(
+OQF-8425 == QQF-8425 ? False
+Received 10 image(s)
+Read 6 license plates
+Read 5 license plates correctly
+60% of license plates were read
+83% of license plates were read correctly
+python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      457.11s user 4.63s system 135% cpu 5:41.11 total
