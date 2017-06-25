0fadb0d630e04ff0c72c3be80e5369049a4ebc85
Matthias Nunes <matthiasnunes@gmail.com>
2017-06-24 22:30:30 -0300

    adds results for pipeline on mac executable

--- mac_pipeline_results.exs
+++ mac_pipeline_results.exs
@@ -1,70 +1,12 @@
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      68.50s user 2.33s system 519% cpu 13.634 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      68.67s user 2.38s system 517% cpu 13.728 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      70.95s user 2.33s system 524% cpu 13.968 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      72.45s user 2.36s system 523% cpu 14.284 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      72.75s user 2.35s system 530% cpu 14.164 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      72.84s user 2.34s system 528% cpu 14.234 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      73.47s user 2.37s system 529% cpu 14.319 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      73.16s user 2.37s system 528% cpu 14.289 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      73.25s user 2.35s system 528% cpu 14.300 total
-ITA-0204
-IVA-5837
-IVG-3501
-IWU-3070
-IXJ-6605
-QQF-8425
-python -O src/main.py input/photos/IPN-5186.JPG input/photos/ITA-0204.JPG      72.92s user 2.36s system 529% cpu 14.229 total
+13.634
+|> Kernel.+(13.728)
+|> Kernel.+(13.968)
+|> Kernel.+(14.284)
+|> Kernel.+(14.164)
+|> Kernel.+(14.234)
+|> Kernel.+(14.319)
+|> Kernel.+(14.289)
+|> Kernel.+(14.300)
+|> Kernel.+(14.229)
+|> Kernel./(10)
+|> IO.puts
